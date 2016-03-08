da7fac57a1bfd50e98ee7601faacf72649dc7725
Michał Jereczek <jereczem@vps235120.ovh.net>
2016-03-08 23:23:56 +0100

    cw3

--- cw3/cw3.c
+++ cw3/cw3.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+
 #define BUFFOR_SIZE 80
+#define CHILDREN_NUMBER 5
 
 /*
 	Sums numbers in vector.
@@ -17,7 +25,7 @@ double sum(double* vector, int vector_length) {
 /*
 	Print vector informations.
 */
-void print_vector(double *vector, int vector_length){
+void print_vector(double *vector, int vector_length) {
 	int i;
 	
 	printf("Vector has %d elements\n", vector_length);
@@ -31,7 +39,7 @@ void print_vector(double *vector, int vector_length){
 /*
 	Return readed vector. Vector lenght as pointer.
 */
-double* read_vector(int *vector_length){
+double* read_vector(int *vector_length) {
 	FILE* f = fopen("vector.dat", "r");
 	
     char buffor[BUFFOR_SIZE+1];
@@ -50,17 +58,79 @@ double* read_vector(int *vector_length){
 	return vector;
 }
 
+/*
+	Operation after receiving USR1 signal.
+*/
+void on_usr1(int signal) {
+	printf("%d: Otrzymalem USR1\n", getpid());
+	exit(0);
+}
+
+/*
+	Setting management of USR1 signal
+*/
+void set_usr1_signal() {
+	sigset_t mask;
+	
+	struct sigaction usr1;
+	sigemptyset(&mask); 
+	usr1.sa_handler = (&on_usr1);
+	usr1.sa_mask = mask;
+	usr1.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &usr1, NULL);
+}
+
+/*
+	Creates children and sets operation for USR1 signal.
+*/
+void create_children() {
+	int child_id;
+	
+	for(child_id=0; child_id<CHILDREN_NUMBER; child_id++){
+		pid_t pid = fork();
+		if(pid < 0) {
+			printf("Error");
+			exit(1);
+		} else if (pid == 0) {
+			printf("Child\t (%d): %d\n", child_id + 1, getpid());
+			set_usr1_signal();
+			while(1) 
+				pause();
+		} else  {
+			 printf("Parent\t (%d): %d\n", child_id + 1, getpid());
+		}
+	}
+}
+
+/*
+	Waits for all children to be closed.
+*/
+void wait_for_children() {
+	int j=0;
+	
+	for(j=0; j<CHILDREN_NUMBER; j++){
+					 wait(NULL);
+	}
+}
 
 
 int main(int argc, char **argv) { 
 
+	int vector_length;
+	double* vector; 
+	double vector_sum;
 	
-	int vector_length ;
-	double* vector = read_vector(&vector_length);
-	double vector_sum = sum(vector, vector_length);
+	create_children();
 	
+	vector = read_vector(&vector_length);
+	vector_sum = sum(vector, vector_length);
+		
 	print_vector(vector, vector_length);
+	
+	wait_for_children() ;
+	
 	printf("Suma elementow w wektorze = %f\n", vector_sum );
+
 	
 	return 0;
 }
