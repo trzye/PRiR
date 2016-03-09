31ac307c21e64665936044f4dc73fa25c6b1cc2c
Michał Jereczek <jereczem@vps235120.ovh.net>
2016-03-09 01:40:45 +0100

    zad3

--- cw3/cw3.c
+++ cw3/cw3.c
@@ -6,9 +6,37 @@
 #include <signal.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #define BUFFOR_SIZE 80
 #define CHILDREN_NUMBER 5
+#define MEMORY_KEY 123
+
+int   shmid;
+char* shmaddr;
+double* vector; 
+int child_id;
+
+void save_data_to_shared_memory(int key, char* data){
+	if( (shmid = shmget(key, BUFFOR_SIZE, 0666 | IPC_CREAT)) < 0)
+		printf("shmget error");
+		
+	if( (shmaddr = shmat(shmid, 0, 0)) < 0)
+		printf("shmat error");
+		
+	strcpy (shmaddr, data);
+}
+
+char* read_data_from_shared_memory(int key){
+	if( (shmid = shmget(key, BUFFOR_SIZE, 0666 | IPC_CREAT)) < 0)
+		printf("shmget error");
+		
+	if( (shmaddr = shmat(shmid, 0, 0)) < 0)
+		printf("shmat error");
+		
+	return shmaddr;
+}
 
 /*
 	Sums numbers in vector.
@@ -59,10 +87,33 @@ double* read_vector(int *vector_length) {
 }
 
 /*
+	Writes vector indexes to shared memory
+*/
+void write_vector_indexes(int vector_length) {
+	char buffor[BUFFOR_SIZE+1];
+	int interval = vector_length / CHILDREN_NUMBER;
+	int j;
+	for(j=0; j<CHILDREN_NUMBER*2; j=j+2){
+		sprintf(buffor, "%d", interval * j);
+		save_data_to_shared_memory(j, buffor );
+		if(j == (CHILDREN_NUMBER*2 - 6)){
+			sprintf(buffor, "%d", vector_length);
+			save_data_to_shared_memory(j + 1, buffor );
+		} else {
+			sprintf(buffor, "%d", interval * (j + 1));
+			save_data_to_shared_memory(j + 1, buffor );
+		}
+	}
+}
+
+/*
 	Operation after receiving USR1 signal.
 */
 void on_usr1(int signal) {
-	printf("%d: Otrzymalem USR1\n", getpid());
+	printf("%d: Otrzymalem USR1\n", getpid());	
+	int index1 = atoi(read_data_from_shared_memory(child_id ));
+	int index2 = atoi(read_data_from_shared_memory(child_id + 1));
+	printf("%d: Moje indexy: [%d:%d]]\n",getpid(), index1, index2);	
 	exit(0);
 }
 
@@ -105,8 +156,6 @@ void set_usr2_signal() {
 	Creates children and sets operation for USR1 signal.
 */
 void create_children(int parent_pid) {
-	int child_id;
-	
 	for(child_id=0; child_id<CHILDREN_NUMBER; child_id++){
 		pid_t pid = fork();
 		if(pid < 0) {
@@ -136,14 +185,13 @@ void wait_for_children() {
 	}
 }
 
-
 int main(int argc, char **argv) { 
 
 	pid_t parent_pid = getpid() ;
 
 	int vector_length;
-	double* vector; 
 	double vector_sum;
+	int j;
 	
 	set_usr2_signal();
 	create_children(parent_pid);
@@ -152,11 +200,15 @@ int main(int argc, char **argv) {
 	if(getpid() == parent_pid) {
 		
 		vector = read_vector(&vector_length);
+		write_vector_indexes(vector_length) ;
 		vector_sum = sum(vector, vector_length);
 		print_vector(vector, vector_length);
 		printf("Suma elementow w wektorze = %f\n", vector_sum );
 		
-		//kill(parent_pid+1, SIGUSR1);
+		for(j=1; j<=CHILDREN_NUMBER; j++){
+			kill(parent_pid+j, SIGUSR1);
+		}
+		
 		wait_for_children();
 		
 		return 0;
